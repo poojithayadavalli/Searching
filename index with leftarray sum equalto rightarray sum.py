06eaa27713e4e5cfa710c3cd2ce4459d3dc197a6
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-07 09:42:44 +0530

    Create index with leftarray sum equalto rightarray sum.py

---
+++ index with leftarray sum equalto rightarray sum.py
@@ -0,0 +1,50 @@
+"""
+
+Given, an array of size n. Find an element which divides the array in two sub-arrays with equal sum.
+
+Input:
+Firstline indicates the integer n
+Secondline consists of elements of array
+
+Output:
+print the index that divides array into two subarrays with equal sum
+
+Examples:
+
+Input :
+4
+1 4 2 5
+Output : 
+2
+
+Explanation : If 2 is the partition, subarrays are : {1, 4} and {5}
+
+Input : 
+6
+2 3 4 1 4 5
+Output :
+1
+Explanation : If 1 is the partition, 
+ Subarrays are : {2, 3, 4} and {4, 5}
+ 
+ """
+ def findElement(arr, size) : 
+  
+    right_sum, left_sum = 0, 0
+    for i in range(1, size) : 
+        right_sum += arr[i]
+    i, j = 0, 1 
+    while j < size : 
+        right_sum -= arr[j] 
+        left_sum += arr[i] 
+  
+        if left_sum == right_sum : 
+            return arr[i + 1]
+        j += 1
+        i += 1
+  
+    return -1
+n=int(input())
+arr=list(map(int,input().split()))
+findElement(arr,n)
+
